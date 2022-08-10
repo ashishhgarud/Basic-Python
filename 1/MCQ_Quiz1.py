cc0b2613b518feae133122c404e3d7f0b8050108
Ashish H Garud <57317635+ashishhgarud@users.noreply.github.com>
2022-08-10 14:58:25 +0530

    Delete 1 directory

--- 1/MCQ_Quiz1.py
+++
@@ -1,7 +0,0 @@
-from quiz_template import quiz
-q = [question 
-
-]
-def quiz():
-    score = 0
-    for q in question
--- 1/quiz_template.py
+++
@@ -1,6 +0,0 @@
-class quiz:
-    def __int__(self, question, multiple_choice, user_choice, answer):
-        self.question = question
-        self.multiple_choice = multiple_choice
-        self.user_choice = user_choice
-        self.answer = answer
